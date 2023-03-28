05beda79bb34427c41b723d4fc01573b59370396
Dhaval Patel <dhavalpatel32768@gmail.com>
2023-03-28 07:54:26 +0000

    changed properties to Map type

--- org-roam-server/lib/src/models/node.dart
+++ org-roam-server/lib/src/models/node.dart
@@ -1,18 +1,17 @@
 import 'utils.dart';
-import 'node_properties.dart';
 class Node {
   final String id;
   final List<String> tags;
-  final NodeProperties properties;
+  final Map<String,dynamic> properties;
   final List<String>? olp;
   final int pos;
   final int level;
   final String title;
   final String file;
 
-	String get type => properties.type ?? 'None';
-	String get genre => properties.genre ?? 'None';
-	String get space => properties.space ?? 'None';
+	String get type => properties['NEURON_TYPE'] ?? 'None';
+	String get genre => properties['NEURON_GENRE'] ?? 'None';
+	String get space => properties['NEURON_SPACE'] ?? 'None';
 
   const Node({
     required this.id,
@@ -31,7 +30,7 @@ class Node {
         : [];
     return Node(
       id: trimQuotes(json['id']),
-      properties: NodeProperties.fromJson(json['properties']),
+      properties: json['properties'],
       tags: tags.cast<String>(),
       olp: json['olp']?.cast<String>(),
       pos: json['pos'],
@@ -44,7 +43,7 @@ class Node {
   Map<String, dynamic> toJson() => {
         'id': id,
         'tags': tags,
-        'properties': properties.toJson(),
+        'properties': properties,
         'olp': olp,
         'pos': pos,
         'level': level,
